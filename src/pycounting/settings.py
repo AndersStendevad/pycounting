34ed6a429af69063ab899c373fb0a79377f2bb53
andst <andst@itu.dk>
2022-03-22 14:13:57 +0100

    made setup_settings work

--- src/pycounting/settings.py
+++ src/pycounting/settings.py
@@ -1,16 +1,23 @@
+""" Module for holding the settings for pycounting """
+
 from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    test: str 
+    """Global Settings"""
+
+    test: str
 
     class Config:
-        env_file = 'settings.txt'
-        env_file_encoding = 'utf-8'
+        """Used to grab specific settings form settings.txt file"""
+
+        env_file = "settings.txt"
+        env_file_encoding = "utf-8"
 
     @classmethod
     def setup_settings(cls):
+        """Classmethod to interactivly create a settings.txt file"""
         with open("settings.txt", "w") as file:
-            for var in dict(cls).keys():
+            for var, info in cls.__dict__["__fields__"].items():
                 value = input("Set value for {var}")
                 file.write(f"{var}={value}\n")
